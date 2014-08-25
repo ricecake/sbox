19dd03014e265b4742795cf9f1692f792c59b1a0
Sebastian Green-Husted <ricecake@tfm.nu>
2014-08-25 19:26:45 -0400

    flesh out the util file a bit

--- src/sbox_utils.erl
+++ src/sbox_utils.erl
@@ -1,5 +1,21 @@
 -module(sbox_utils).
 
--export([init_table/2]).
+-export([init/1, init_table/2]).
 
-init_table(Table, _Opts) -> {ok, Table}.
+init_table(Table, Opts) ->
+	ok = case mnesia:create_table(Table, Opts) of
+		{atomic, ok} -> ok;
+		{aborted, {already_exists, Table}} -> ok;
+		{aborted, Reason} -> Reason
+	end,
+	{ok, Table}.	
+
+init([]) ->
+	Node = node(),
+	ok = case mnesia:create_schema([Node]) of
+		ok -> ok;
+		{error, {Node, {already_exists, Node}}} -> ok;
+		_ -> error
+	end,
+	mnesia:start().
+
