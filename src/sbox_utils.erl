63ca7d31c428e4279c6afb331668c76437650c22
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2014-08-26 20:47:07 -0400

    add some util methods for encrypt, decrypt data, and key generation

--- src/sbox_utils.erl
+++ src/sbox_utils.erl
@@ -1,6 +1,6 @@
 -module(sbox_utils).
 
--export([init/1, init_table/2]).
+-export([init/1, init_table/2, pad/2, crypt/2, decrypt/2, mk_key/1]).
 
 init_table(Table, Opts) ->
 	ok = case mnesia:create_table(Table, Opts) of
@@ -19,3 +19,19 @@ init([]) ->
 	end,
 	mnesia:start().
 
+mk_key(PassPhrase) when is_binary(PassPhrase) -> crypto:hash(sha256, PassPhrase).
+
+pad(Width,Binary) when size(Binary) rem Width =:= 0 -> Binary;
+pad(Width,Binary) -> <<Binary/binary, (crypto:rand_bytes(Width - (size(Binary) rem Width)))/bits >>.
+
+
+crypt(Key, Data) ->
+	Prepped = << 16#deadbeef:32, (size(Data)):32, Data/binary >>,
+	Packed  = pad(16, Prepped),
+	Iv      = crypto:rand_bytes(16),
+	Cipher  = (crypto:block_encrypt(aes_cbc256, Key, Iv, Packed)),
+	{ok, << Iv:128/bits, Cipher/binary >>}.
+
+decrypt(Key, <<Iv:128/bits, Cipher/binary>>) ->
+	<< 16#deadbeef:32, Size:32, PlainText:Size/binary, _Padding/bits >> = crypto:block_decrypt(aes_cbc256, Key, Iv, Cipher),
+	{ok, PlainText}.
