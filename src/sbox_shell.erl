1dd3bad095fb1f5fea326f7a7e5aa3046b6b97d7
Sebastian Green-Husted <ricecake@tfm.nu>
2014-09-21 19:12:49 -0400

    actually use new function

--- src/sbox_shell.erl
+++ src/sbox_shell.erl
@@ -43,6 +43,7 @@ shell_loop({User, {IP, _Port} = Peer}, Auth) ->
 
 eval_cli(Line, _Auth) ->
 	{ok, Tokens, _} = sbox_command_tokenizer:string(Line),
-	sbox_command_parser:parse(Tokens).
+	process(sbox_command_parser:parse(Tokens)).
+
 
 process({Command, _Details}) -> {unknown, Command}.
